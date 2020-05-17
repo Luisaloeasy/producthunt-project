9defdec3c9452314a64e62036bcf2edb1f0b7799
Luis Aloisi <luisaloeasy@gmail.com>
2020-05-17 01:34:25 -0400

    Termine la primera parte del 3er proyecto con Auth

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -2,10 +2,13 @@
 from django.contrib import admin
 from django.urls import path,include
 from products import views #Debo importar los views de las app que este llamando
+from django.conf.urls.static import static
+from django.conf import settings
 
 #Son las direcciones a las que se admiten en la pag web
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'), #Nos lleva al homepage
-    path('accounts/', include('accounts.urls')),  
-]
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),  
+] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
